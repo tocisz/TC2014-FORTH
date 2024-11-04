b74cbb82ca480f73283195053053989a773429e0
Tomasz Cichocki <cichymail@gmail.com>
2024-11-04 19:00:41 +0100

    separate commands to ensure flush

--- upload.sh
+++ upload.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+z80cat() {
+	tr "\n" "\r" | nc 192.168.0.46 23
+}
 curl -X POST http://192.168.0.46/console/reset
 docker exec z80 bash -c "cd ~/RC2014-FORTH; make"
-(
-echo -n C
+echo -n C | z80cat
 sleep 0.2
-echo 33792
+echo 33792 | z80cat
 sleep 0.2
-echo hload
-cat ram.hex
-echo "?usr(0)"
-) | tr "\n" "\r" | nc 192.168.0.46 23
+echo hload | z80cat
+cat ram.hex | z80cat
+echo "?usr(0)" | z80cat
