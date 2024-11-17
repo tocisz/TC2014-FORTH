c8bf68ea85c1d8ee0697409f0b916e4794b017c5
Tomasz Cichocki <cichymail@gmail.com>
2024-11-17 13:48:46 +0100

    todos using linked list

---
+++ scripts/todos2.f
@@ -0,0 +1,51 @@
+: cons ( t h -> addr )
+	here >r , , r>
+;
+
+: head @ ;
+: head@ @ @ ;
+: tail 2+ @ ;
+
+variable todoL
+variable todoN
+
+: .todo
+	dup c@
+	dup 127 and 3 .r ." . "
+	128 and if ." [x] " else ." [ ] " then
+	1+ count 127 and type cr
+;
+
+: todos
+	todoL @
+	cls
+	begin
+		dup while
+		dup head .todo
+		tail
+	repeat
+	drop
+;
+
+: todo
+	here
+	todoN @ 1+ dup c, todoN ! ( counter )
+	34 word c@ 1+ allot ( message )
+	todoL @ swap cons todoL ! ( store it )
+	todos
+;
+
+: check ( n - )
+	todoL @
+	begin
+		dup while
+		dup head c@ 127 and ( n todo m )
+		3 pick = if
+			dup head ( n todo head )
+			128 toggle ( n todo )
+		then
+		tail
+	repeat
+	2drop
+	todos
+;
